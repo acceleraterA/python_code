ca717d295dd844f6833a48564f496adf75ab26d6
acceleraterA <36606218+acceleraterA@users.noreply.github.com>
2020-09-25 14:11:01 -0700

    Update combine_csv.py

--- combine_csv.py
+++ combine_csv.py
@@ -2,15 +2,12 @@ import glob
 import pandas as pd
 
 # get data file names
-path =r'C:\Users\62547\PycharmProjects\input files'# please change to your local path
+path =r'C:\Users\62547\PycharmProjects\input files'
 filenames = glob.glob(path + "/*.csv")
-
-dfs=[]
+dfs=[pd.read_csv('s1 - Sheet1.csv', usecols=[0])]
 for filename in filenames:
-   # data=pd.read_csv(filename)
-
-    dfs.append( pd.read_csv(filename))
-
+    data=pd.read_csv(filename, usecols=[1])
+    dfs.append(data)
 # Concatenate all data into one DataFrame
 big_frame = pd.concat(dfs, axis=1, ignore_index=False)
 big_frame.to_csv('out.csv')
